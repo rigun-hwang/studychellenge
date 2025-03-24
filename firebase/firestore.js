1972d79950945c3849bcabd75f12587f23d86b99
rigun-hwang <rigun.hwang@gmail.com>
2025-03-24 20:19:54 +0900

    Update firestore.js

--- firebase/firestore.js
+++ firebase/firestore.js
@@ -1,4 +1,6 @@
-import app from "@/firebase/firebasedb"
+/*eslint-disable */
+
+import {app, auth} from "./firebase/firebasedb"
 import {getFirestore} from "firebase/firestore"
 
 const fireStore = getFirestore(app)
